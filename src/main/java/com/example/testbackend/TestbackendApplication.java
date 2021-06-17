5a67865d17f98c1d39533a0b86d1655249295889
thisisminh172 <thisisminh172@gmail.com>
2021-06-17 22:32:10 +0700

    first commit

---
+++ src/main/java/com/example/testbackend/TestbackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.testbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestbackendApplication.class, args);
+	}
+
+}
